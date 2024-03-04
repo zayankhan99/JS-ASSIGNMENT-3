9fec18107de52364c8aebe59fc7e7c7cc5fcaeff
ZAYAN KHAN <145120648+zayankhan99@users.noreply.github.com>
2024-03-04 23:22:31 +0500

    Add files via upload

---
+++ JS ASSIGNMENT 3/app.js
@@ -0,0 +1,184 @@
+// chapter 9-10
+// task 1
+
+// var city = prompt("Enter Your City Name","city");
+// if (city == "karachi") {
+//     console.log("Welcome to the city of lights"
+//     )
+// }
+
+//  task 2
+
+// var gender = prompt("Enter Your Gender", "gender");
+// if (gender == "male") {
+//     console.log("Good Morning Sir.")
+// }
+// else if (gender == "female") {
+//     console.log("Good Morning Ma’am."
+//     )
+// }
+
+// task 3
+
+// var signal = prompt("Enter Signal Color");
+// if (signal == "red") {
+//     console.log("Must Stop")
+// }
+// if (signal == "yellow") {
+//     console.log("Ready to move")
+// }
+// if (signal == "green") {
+//     console.log("Move now")
+// }
+
+//  task 4
+
+// var fuel = prompt("Enter Remaining Fuel");
+// if(fuel < 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+// else if (fuel > 0.25){
+//    alert("Your car has enough fuel")
+// }
+
+//  task 5
+// a
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+//  b
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is false");
+// }
+
+// c
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+//  this condition is false
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+//  this condition is true
+// if (++c < 14) {
+//     alert("condition 3 is true");
+//     // this condition is false 
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+// }
+// this codition is true
+
+//  d
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+//  e
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+//   true
+
+//  f
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+//  false
+
+// task 6
+
+
+// var subject1 = prompt("Enter marks obtained in subject 1:");
+// var subject2 = prompt("Enter marks obtained in subject 2:");
+// var subject3 = prompt("Enter marks obtained in subject 3:");
+// var totalmarks = prompt("Enter total marks:");
+// var totalmarksobtained = subject1 + subject2 + subject3;
+// var percentage = (totalmarksobtained / totalMarks) * 100;
+
+// if (percentage >= 90) {
+//     grade = 'A+';
+// } else if (percentage >= 80) {
+//     grade = 'A';
+// } else if (percentage >= 70) {
+//     grade = 'B';
+// } else if (percentage >= 60) {
+//     grade = 'C';
+// } else if (percentage >= 50) {
+//     grade = 'D';
+// } else {
+//     grade = 'F';
+// }
+
+// document.write("<h2>Result:</h2>");
+// document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+// document.write("<p>Grade: " + grade + "</p>");
+
+// task 7
+
+
+// var secretnum = ("5");
+// var secretguess = prompt("guess the secret num")
+// if(secretguess === secretnum)
+// alert("Bingo! Correct answer");
+// else if (secretguess + 1 === secretnum ){
+//     alert ("Close enough to the correct answer")
+// }
+
+
+
+//  task 10
+
+// var temp = +prompt("temperature here ");
+// if(temp > 40){
+//     document.write("Its too hot outside")
+// } 
+// else if(temp > 30){
+//     document.write("the weather today is normal")
+// }
+// else if (temp > 20 ){
+//     document.write("todays weather is beautiful")
+// }
+// else if (temp > 10 ){
+//     document.write("OMG! Todays weather is so beautiful")
+
+// }
+
+// task 11
+
+// var num1 = +prompt("firstNumber");
+// var opt = prompt("operator");
+// var num2 = +prompt('SecondNumber');
+
+// if (opt == "+") {
+//     document.write(num1 + num2)
+// }
+
+// else if (opt == "-") {
+//     document.write(num1 - num2)
+// }
+
+// else if (opt == "*") {
+//     document.write(num1 * num2)
+// }
+
+// else if (opt == "/") {
+//     document.write(num1 / num2)
+// }
+
+
